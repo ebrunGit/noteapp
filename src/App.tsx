f71cd20465bd40961f247a7a9de02e9672556a31
Emmanuel <emmanuelbrun4@gmail.com>
2021-03-23 15:45:13 +0100

    adding moment dependency ; folder organisation and creation of ui and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import FirstView from "./components/pages/firstView";
 
 export default function App() {
   return (
     <div className="App">
+      <FirstView />
     </div>
   );
 }
